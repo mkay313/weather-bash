3f6ef5c86264d017b3ab977355ed7b98e88ebd6f
mkay313 <mkmozgawa@gmail.com>
2018-11-11 22:44:03 +0100

    cleanup

--- test_weather.py
+++ test_weather.py
@@ -1,15 +1,12 @@
-
 #imports
 import unittest
-from weather import form_url, save_key_to_file, read_key_from_file
-
-APIXU_BASE_URL = 'https://api.apixu.com/v1/current.json?key='
+from weather import form_url, save_key_to_file
 
 class WeatherTestCase(unittest.TestCase):
     """Tests for `weather.py`."""
 
     def test_url_formation(self):
-        self.assertEqual(form_url("2f0115073b344ca7be6194156171611", "Poznan"), "https://api.apixu.com/v1/current.json?key=2f0115073b344ca7be6194156171611&q=Poznan") 
+        self.assertEqual(form_url("2f0115073b344ca7be6194156171611", "Poznan"), "https://api.apixu.com/v1/forecast.json?key=2f0115073b344ca7be6194156171611&q=Poznan") 
 
     def test_save_key_to_file(self):
         save_key_to_file("magic_key", "file.txt")
