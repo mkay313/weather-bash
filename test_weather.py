6c338207d4135bc45f525df9625c878763ef6cdb
mkay313 <mkmozgawa@gmail.com>
2018-11-11 21:02:42 +0100

    adds second test, adds key handling

--- test_weather.py
+++ test_weather.py
@@ -1,8 +1,7 @@
 
-
 #imports
 import unittest
-from weather import form_url
+from weather import form_url, save_key_to_file
 
 APIXU_BASE_URL = 'https://api.apixu.com/v1/current.json?key='
 
@@ -11,6 +10,11 @@ class WeatherTestCase(unittest.TestCase):
 
     def test_url_formation(self):
         self.assertEqual(form_url("2f0115073b344ca7be6194156171611", "Poznan"), "https://api.apixu.com/v1/current.json?key=2f0115073b344ca7be6194156171611&q=Poznan") 
+
+    def test_save_key_to_file(self):
+        save_key_to_file("magic_key", "file.txt")
+        with open('file.txt') as file: key = file.read()
+        self.assertEqual("magic_key", key)
 
 if __name__ == '__main__':
     unittest.main()
--- weather.py
+++ weather.py
@@ -3,14 +3,17 @@
 #imports
 import argparse
 import json
+import sys
 from urllib.request import urlopen
 
-APIXU_BASE_URL = 'https://api.apixu.com/v1/current.json?key=' 
+APIXU_BASE_URL = 'https://api.apixu.com/v1/current.json?key='
+APIXU_FILE_NAME = 'apixukey.txt'
 
 def parse_args():
     parser = argparse.ArgumentParser(description='Why look out of the window when you can get the weather info right in the terminal?')
     parser.add_argument('-l', '--location', default="Poznan", help="set city name")
     parser.add_argument('-e', help="get extra advice", action="store_true")
+    parser.add_argument('-k', '--key', help="set apixu key")
     return parser.parse_args()
 
 def form_url(key, location):
@@ -23,5 +26,22 @@ def get_data(url):
 def give_advice():
     pass
 
+def save_key_to_file(key, filename):
+    with open(filename, 'w') as file:
+        file.write(key)
+        file.close()
+
 args = parse_args()
 print(args)
+
+if args.key:
+   save_key_to_file(args.key, APIXU_FILE_NAME)
+else:
+    try:
+        with open(APIXU_FILE_NAME) as file: key = file.read()
+    except FileNotFoundError:
+        print("Apixu key not found. Try rerunning the script with -k <your apixu key>.")
+        sys.exit(1)
+
+
+
